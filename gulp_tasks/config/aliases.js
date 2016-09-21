03828fbd2a8e58526365244dad84d7a08952de18
Maxim Orlov <mail@orlovmax.com>
2016-09-21 22:11:53 +0300

    template configured

--- gulp_tasks/config/aliases.js
+++ gulp_tasks/config/aliases.js
@@ -5,8 +5,8 @@ var gulp = require('gulp'),
 // Install bower dependencies and place them to dev folders
 gulp.task('start', function() {
 	runSequence(
-		'shell:bower',
-		'bower:main',
+		// 'shell:bower',
+		// 'bower:main',
 		'clean:gitkeep'
 	);
 });
@@ -14,9 +14,9 @@ gulp.task('start', function() {
 // Dev task with static server
 gulp.task('dev', function() {
 	runSequence(
-		'coffee:main',
+		// 'coffee:main',
 		'rigger:main',
-		'sass:main',
+		// 'sass:main',
 		'stylus:main',
 		'postcss:dev',
 		'cmq:main',
@@ -43,9 +43,9 @@ gulp.task('build', function() {
 // Regenerate and build project by running all tasks
 gulp.task('rebuild', function() {
 	runSequence(
-		'coffee:main',
+		// 'coffee:main',
 		'rigger:main',
-		'sass:main',
+		// 'sass:main',
 		'stylus:main',
 		'postcss:dev',
 		'cmq:main',
